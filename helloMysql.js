6dd8c1798217d1eafc148a5b63275801a6d88d31
kimyoo <kimyoo@oregonstate.edu>
2016-06-01 21:12:11 -0500

    7

--- helloMysql.js
+++ helloMysql.js
@@ -1,181 +1,115 @@
-var express = require('express'); 
-var mysql = require('./dbcon.js'); 
+var express = require('express');
+var mysql = require('./dbcon.js');
 
-var app = express(); 
-
-
+var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
-//set port
 app.set('port', 3000);
-
-//use this directory
 app.use(express.static('public'));
 
-//route path
+
 app.get('/',function(req,res,next){
-	
-	//create context object
-	var context = {};
-	
-	//run select query
-	mysql.pool.query('SELECT * FROM workout', function(err, rows, fields){
+  var context = {};
+  mysql.pool.query('SELECT * FROM workout', function(err, rows, fields){
     if(err){
       next(err);
       return;
-    }		
-		//set the results to the rows object
-		context.results = rows;
-		
-		//render the context
-		res.render('home', context);
-	});	
+    }
+    //context.results = JSON.stringify(rows);
+    context.results = rows;
 	
-	//print url to console
-	console.log(req.originalUrl);
+    res.render('home', context);
+  });
 });
 
-//add row
+
 app.get('/insert',function(req,res,next){
-	
-	//create context object
-	var context = {};
-	
-	//run insert query
-	mysql.pool.query("INSERT INTO workout (`name`,`rep`,`weight`,`date`,`measure`) " +
+  var context = {};
+  	mysql.pool.query("INSERT INTO workout (`name`,`rep`,`weight`,`date`,`measure`) " +
 					"VALUES (?, ?, ?, ?, ?)",
 					[req.query.name, req.query.rep, req.query.weight, req.query.date, req.query.measure],
 					function(err, result){
-	
-	//check for erros
-	if(err){
-		next(err);
-		return;
-	}
-	
-	//print url to console
-	console.log(req.originalUrl);
-	
-	//show root
-	res.redirect("/");
+    if(err){
+      next(err);
+      return;
+    }
+    //res.render('home',context);
+	res.redirect("http://ec2-52-37-250-27.us-west-2.compute.amazonaws.com:3000/");
   });
 });
 
-//delete row
 app.get('/delete',function(req,res,next){
-	
-	//create context object
-	var context = {};
-	
-	//run delete query
-	mysql.pool.query("DELETE FROM workout WHERE id=?",
-					[req.query.id],
-					function(err, result){
-	
-	//check for errors
-	if(err){
-		next(err);
-		return;
-	}
-	
-	//print url to console
-	console.log(req.originalUrl);
-	
-	//show root
-	res.redirect("/");
+  var context = {};
+  mysql.pool.query("DELETE FROM workout WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    //context.results = "Deleted " + result.changedRows + " rows.";
+    //res.render('home',context);
+	res.redirect("http://ec2-52-37-250-27.us-west-2.compute.amazonaws.com:3000/");
   });
 });
 
-//update a record
-app.get('/update',function(req,res,next){
-	
-	//create context object
-	var context = {};
-  
-	//print url to console
-	console.log(req.originalUrl);
-	
-	//run select query on this id
-	mysql.pool.query("SELECT * FROM workout WHERE id=?", [req.query.id], function(err, result){
-		
-		//check for errors
-		if(err){
-		  next(err);
-		  return;
-		}
-		
-		//if result comes back...
-		if(result.length == 1){
-			
-			//set variable to result
-			var curVals = result[0];
-			
-			//run update query
-			mysql.pool.query("UPDATE workout SET name=?, rep=?, weight=?, date=?, measure=? WHERE id=? ",
-				[req.query.name || curVals.name, 
-				req.query.rep || curVals.rep, 
-				req.query.weight || curVals.weight, 
-				req.query.date || curVals.date, 
-				req.query.measure || curVals.measure, 
-				req.query.id],
-				
-				//check for errors and show root
-				function(err, result){
-					if(err){
-						next(err);
-						return;
-					}
-					
-					//show root
-					res.redirect("/");
-				}
-			);
-		}
-  });
-});
 
-//edit a record
+
+
+///safe-update?id=1&name=The+Task&done=false
 app.get('/edit',function(req,res,next){
-
-	//create context object
-	var context = {};
-	
-	//run select query
-	mysql.pool.query("SELECT * FROM workout WHERE id=?", [req.query.id], function(err, rows, fields){
-		
-		//set the results to the rows object
-		context.rowToEdit = rows[0];
-		
-		//render the context
-		res.render('update', context);
-	});	
-  
-	//print url to console
-	console.log(req.originalUrl);
+  var context = {};
+  mysql.pool.query("SELECT * FROM workout WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    if(result.length == 1){
+      var curVals = result[0];
+			mysql.pool.query("UPDATE workout SET name=?, rep=?, weight=?, date=?, measure=? WHERE id=? ",
+				[req.query.name || curVals.name, req.query.rep || curVals.rep, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.measure || curVals.measure, 
+				req.query.id],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+        //context.results = "Updated " + result.changedRows + " rows.";
+        res.redirect("http://ec2-52-37-250-27.us-west-2.compute.amazonaws.com:3000/");
+      });
+    }
+  });
 });
 
-//reset table from the instructor
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS workout", function(err){ //replace your connection pool with the your variable containing the connection pool
-	
-	
-    var createString = "CREATE TABLE workout("+
-    "id INT PRIMARY KEY AUTO_INCREMENT,"+
-    "name VARCHAR(255) NOT NULL,"+
-    "rep INT,"+
-    "weight INT,"+
-    "date DATE,"+
+  mysql.pool.query("DROP TABLE IF EXISTS workout", function(err){
+    var createString = "CREATE TABLE workout(" +
+    "id INT PRIMARY KEY AUTO_INCREMENT," +
+    "name VARCHAR(255) NOT NULL," +
+	"rep INT," +
+	"weight INT," +
+	"date DATE," +
     "measure BOOLEAN)";
     mysql.pool.query(createString, function(err){
-      context.results = "Table reset!";
-      res.render('resetTable',context);
+      context.results = "Table reset";
+      res.render('home',context);
     })
   });
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started at http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
 });
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+ 
